e48233f223716b5df63cee385abfa076443eda8b
Jeffer <jefferdcondiza@gmail.com>
2025-03-25 10:31:40 -0500

    Refactory

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,16 +4,22 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+
+  NTAS_SERVERS: string[];
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
+    NTAS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NTAS_SERVERS: process.env.NTAS_SERVERS?.split(','),
+});
 if (error) {
   throw new Error(`Config validation error ${error.message}`);
 }
@@ -23,4 +29,5 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   databaseUrl: envVars.DATABASE_URL,
+  natsServers: envVars.NTAS_SERVERS,
 };
